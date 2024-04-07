e7e4a028809cb8764ff5cc60e1beced4dde03ebd
Malindu Mahara <59385501+malindu20@users.noreply.github.com>
2024-04-07 18:58:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
-def hello_cloud():
-  return 'Hello from Mahara ECS Container.'
-
-app.run(host='0.0.0.0')
+def main_route():
+    return 'Main route data display'
+ 
+@app.route('/host')
+def host_route():
+    return 'Host route data display'
+ 
+@app.route('/ip')
+def ip_route():
+    # Get client's IP address from the request object
+    client_ip = request.remote_addr
+    return f'IP route data display. Client IP: {client_ip}'
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
